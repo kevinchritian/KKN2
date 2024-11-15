2d74cf2404d1bf3d2c7db517de54ffda142829dc
Kevin Christian -71210797 <93351620+kevinchritian@users.noreply.github.com>
2024-11-15 20:50:37 +0700

    Update home.py

--- views/home.py
+++ views/home.py
@@ -16,7 +16,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout='wide')
+
 
 
 
